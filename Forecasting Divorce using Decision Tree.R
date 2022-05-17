2d71744c77b7b9efab6cc38506e10044d0b87250
Mhezhye <93661354+Mhezhye@users.noreply.github.com>
2022-05-17 06:11:07 +0100

    Create Forecasting Divorce using Decision Tree.R
    Add R forecasting divorce

---
+++ Forecasting Divorce using Decision Tree.R
@@ -0,0 +1,50 @@
+setwd("C:/Users/Mhezhye/Desktop/UoS/ASDM/COURSEWORK/Task 1")
+Divorce<-read.csv("divorce.csv", header=TRUE)
+
+names(Divorce)
+head(Divorce)
+tail(Divorce)
+summary(Divorce)
+str(Divorce)
+
+nrow(Divorce)
+ncol(Divorce)
+dim(Divorce)
+
+Divorce$ClassF<-as.factor(Divorce$Class)
+str(Divorce)
+
+set.seed(1234)
+CD<-sample(2, nrow(Divorce), replace=TRUE, prob=c(0.7,0.3))
+CD
+
+Train<-Divorce[CD==1,]
+Validate<- Divorce[CD==2,]
+
+install.packages("party")
+library(party)
+
+Divorce_Tree<-ctree(ClassF~Atr1+Atr2+Atr3+Atr4+Atr5+Atr6+Atr7+Atr8+Atr9+Atr10+Atr11+Atr12
+                    +Atr13+Atr14+Atr15+Atr16+Atr17+Atr18+Atr19+Atr20+Atr21+Atr22+Atr23+Atr24
+                    +Atr25+Atr26+Atr27+Atr28+Atr29+Atr30+Atr31+Atr32+Atr33+Atr34+Atr35+Atr36
+                    +Atr37+Atr38+Atr39+Atr40+Atr41+Atr42+Atr43+Atr44+Atr44+Atr45+Atr46+Atr47
+                    +Atr48+Atr49+Atr50+Atr51+Atr52+Atr53+Atr54, data=Train)
+Divorce_Tree
+print(Divorce_Tree)
+
+plot(Divorce_Tree)
+plot(Divorce_Tree, type="simple")
+
+predict(Divorce_Tree)
+Variables<-table(predict(Divorce_Tree), Train$ClassF)
+print(Variables)
+
+sum(diag(Variables))/sum(Variables)
+1-sum(diag(Variables))/sum(Variables)
+
+Test_pre_data<- table(predict(Divorce_Tree, newdata=Validate), Validate$ClassF)
+print(Test_pre_data)
+
+sum(diag(Test_pre_data))/sum(Test_pre_data)
+1-sum(diag(Test_pre_data))/sum(Test_pre_data)
+
